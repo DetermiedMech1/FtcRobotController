fe8a6f539c2aa2540c3a266a80ea9cc94dabe158
DetermiedMech1 <determiedmech1@gmail.com>
2025-07-08 08:58:19 -0500

    the NullPointerExceptions have made this infeasible i suppose

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -2,13 +2,15 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name = "TeleOp")
 public class TeleOpMode extends LinearOpMode{
 
-    private final DriveSubsystem driveSubsystem = new DriveSubsystem(hardwareMap, telemetry);
+
+
+    private DcMotor leftMotor = hardwareMap.get(DcMotor.class, "leftmotor");
+    private DcMotor rightMotor = hardwareMap.get(DcMotor.class, "rightmotor");
 
     @Override
     public void runOpMode() {
@@ -19,8 +21,36 @@ public class TeleOpMode extends LinearOpMode{
         resetRuntime();
 
         while (opModeIsActive()) {
-            driveSubsystem.splitDrive(gamepad1.left_stick_y, gamepad1.right_stick_y);
+            splitDrive(gamepad1.left_stick_y, gamepad1.right_stick_y);
         }
+    }
+
+    /**
+     * use lSpeed and rSpeed to move each side of the robot independently
+     *
+     * @param lSpeed left side speed
+     * @param rSpeed right side speed
+     */
+    public void splitDrive(double lSpeed, double rSpeed) {
+        leftMotor.setPower(lSpeed * Constants.MotorConstants.motorSpeed);
+        rightMotor.setPower(rSpeed * Constants.MotorConstants.motorSpeed);
+    }
+
+    /**
+     * Basic tank drive
+     *
+     * @param forward forward power
+     * @param turn turn power
+     */
+    public void tankDrive(double forward, double turn) {
+        //TODO test the math here for whether it is actually correct
+        double lSpeed = forward - turn;
+        double rSpeed = forward + turn;
+
+        splitDrive(lSpeed, rSpeed);
+    }
+    public void encoderForward() {
+
     }
 
 
